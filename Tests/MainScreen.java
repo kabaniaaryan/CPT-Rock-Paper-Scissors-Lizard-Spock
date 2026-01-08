6b3bee794417052573d5ade0a6d8c0c15feae7b4
CLee020 <Carter.lee26@ycdsbk12.ca>
2026-01-08 09:32:06 -0500

    imports

--- Tests/MainScreen.java
+++ Tests/MainScreen.java
@@ -1,5 +1,9 @@
 package Tests;
 
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+
 public class MainScreen {
     public static void main(String[] args){
         System.out.println("Hello World");
