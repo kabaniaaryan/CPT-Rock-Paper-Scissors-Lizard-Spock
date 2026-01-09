ccf8b97a789c7536719f3c946da56214f050a94a
AidanTheThe <aidan.brown26@ycdsbk12.ca>
2026-01-09 09:38:13 -0500

    added to game logic

--- Tests/Game1v1.java
+++ Tests/Game1v1.java
@@ -1,12 +1,28 @@
 package Tests;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Game1v1{
     public static void main(String[] args){
+        BufferedReader kb = new BufferedReader(new InputStreamReader(System.in));
         String strP1Choice;
         String strP2Choice;
-        System.out.println("Pick one: Rock, Paper, Scissors, Lizard, Spock");
-        strP1Choice = "Rock";
-        strP2Choice = "Scissors";
+        System.out.println("P1, Pick one: Rock, Paper, Scissors, Lizard, Spock");
+        try{
+            strP1Choice = kb.readLine();
+        }catch(Exception e){
+            System.out.println("Error");
+            strP1Choice = null;
+        }
+        System.out.println("P2, Pick one: Rock, Paper, Scissors, Lizard, Spock");
+        try{
+            strP2Choice = kb.readLine();
+        }catch(Exception e){
+            System.out.println("Error");
+            strP2Choice = null;
+        }
+
         if(strP1Choice.equals(strP2Choice)){
             System.out.println("TIE");
         }else if(strP1Choice.equals("Rock") && (strP2Choice.equals("Scissors") || strP2Choice.equals("Lizard"))){
@@ -29,6 +45,14 @@ public class Game1v1{
             System.out.println("P1 Wins");
         }else if(strP1Choice.equals("Spock") && (strP2Choice.equals("Paper") || strP2Choice.equals("Lizard"))){
             System.out.println("P2 Wins");
+        }else if(strP1Choice == null && strP2Choice != null){
+            System.out.println("P1 chose nothing, P2 Wins");
+        }else if(strP2Choice == null && strP1Choice != null){
+            System.out.println("P2 chose nothing, P1 Wins");
+        }else{
+            System.out.println("Both players chose nothing, TIE");
+        }
+
         }
     }
 }
