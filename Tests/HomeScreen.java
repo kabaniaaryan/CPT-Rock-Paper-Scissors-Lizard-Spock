a02cd3a0d448a81722bccd736051b7623c8f7402
AidanTheThe <aidan.brown26@ycdsbk12.ca>
2026-01-12 10:57:06 -0500

    Edited home screen

--- Tests/HomeScreen.java
+++ Tests/HomeScreen.java
@@ -1,4 +1,4 @@
-
+package Tests;
 
 import java.awt.*;
 import javax.swing.*;
@@ -10,42 +10,42 @@ public class HomeScreen implements ActionListener {
         // Properties
         JFrame theFrame = new JFrame("Rock Paper Scissors Lizard Spock");
         JPanel thePanel = new JPanel();
-        JTextField PlayerTextField1 = new JTextField("P1");
-        JButton theButton1 = new JButton("Rock");
-        JButton theButton2 = new JButton("Paper");
-        JButton theButton3 = new JButton("Scissors");
-        JButton theButton4 = new JButton("Lizard");
-        JButton theButton5 = new JButton("Spock");
-
-        // Array
-        String[] strChoiceNames = {"Rock", "Paper", "Scissors", "Lizard", "Spock"};
-
-        // Math
-
+        JButton serverButton = new JButton("Host Game");
+        JButton clientButton = new JButton("Join Game");
 
         // Methods
         public void actionPerformed(ActionEvent evt) {
-            if(evt.getSource() == theButton1) {
-
-
-            } else if(evt.getSource() == theButton2) {
-
-                
-            } else if(evt.getSource() == theButton3) {
-
-                
-            } else if(evt.getSource() == theButton4) {
-
-                
-            } else if(evt.getSource() == theButton5) {
-
-                
+            if(evt.getSource() == serverButton){
+                System.out.println("Hosting Game");
+            }else if(evt.getSource() == clientButton){
+                System.out.println("Joining Game");
             }
         }
-        // Constructor
-        public HomeScreen() {
-            new HomeScreen();
 
+        // Constructor
+        public HomeScreen(){
+            thePanel.setPreferredSize(new Dimension(1280, 720));
+            thePanel.setLayout(null);
+            theFrame.setContentPane(thePanel);
+            theFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            theFrame.pack();
+            theFrame.setVisible(true);
+
+            serverButton.setSize(880, 100);
+            serverButton.setLocation(200, 500);
+            serverButton.addActionListener(this);
+
+            clientButton.setSize(880, 100);
+            clientButton.setLocation(200, 610);
+            clientButton.addActionListener(this);
+
+            thePanel.add(serverButton);
+            thePanel.add(clientButton);
+        }
+
+        // Main Method
+        public static void main(String[] args){
+            HomeScreen hs = new HomeScreen();
         }
         
     }
