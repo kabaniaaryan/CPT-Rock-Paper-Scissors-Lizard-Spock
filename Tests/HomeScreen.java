e08ad2301de2897cb579134fdfcccf56f54656d8
AidanTheThe <aidan.brown26@ycdsbk12.ca>
2026-01-12 16:33:26 -0500

    Connected HomeScreen.java to TestGameFile.java

--- Tests/HomeScreen.java
+++ Tests/HomeScreen.java
@@ -1,53 +1,34 @@
 package Tests;
 
-import java.awt.*;
-import javax.swing.*;
-import java.awt.event.*;
-import javax.swing.event.*;
+import java.io.IOException;
+import java.io.InputStream;
+import javax.swing.JPanel;
+import java.awt.image.*;
+import javax.imageio.ImageIO;
+import java.awt.Graphics;
 
-public class HomeScreen implements ActionListener {
+public class HomeScreen extends JPanel {
 
         // Properties
-        JFrame theFrame = new JFrame("Rock Paper Scissors Lizard Spock");
-        JPanel thePanel = new JPanel();
-        JButton serverButton = new JButton("Host Game");
-        JButton clientButton = new JButton("Join Game");
+        BufferedImage HomeImage;
 
         // Methods
-        public void actionPerformed(ActionEvent evt) {
-            if(evt.getSource() == serverButton){
-                System.out.println("Hosting Game");
-            }else if(evt.getSource() == clientButton){
-                System.out.println("Joining Game");
-            }
+        public void paintComponent(Graphics g){
+            super.paintComponent(g);
+            g.drawImage(HomeImage, 0, 0, null);
         }
 
         // Constructor
         public HomeScreen(){
-            thePanel.setPreferredSize(new Dimension(1280, 720));
-            thePanel.setLayout(null);
-            theFrame.setContentPane(thePanel);
-            theFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            theFrame.pack();
-            theFrame.setVisible(true);
-
-            serverButton.setSize(880, 100);
-            serverButton.setLocation(200, 500);
-            serverButton.addActionListener(this);
-
-            clientButton.setSize(880, 100);
-            clientButton.setLocation(200, 610);
-            clientButton.addActionListener(this);
-
-            thePanel.add(serverButton);
-            thePanel.add(clientButton);
+            super();
+            InputStream imageStream = null;
+            imageStream = this.getClass().getResourceAsStream("TheHomeScreen.png");
+            try{
+                HomeImage = ImageIO.read(imageStream);
+            }catch(IOException e){
+                System.out.println("Error loading image");
+            }
         }
-
-        // Main Method
-        public static void main(String[] args){
-            HomeScreen hs = new HomeScreen();
-        }
-        
     }
 
    
--- Tests/TestGameFile.java
+++ Tests/TestGameFile.java
@@ -1,5 +1,49 @@
 package Tests;
 
-public class TestGameFile {
-    
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+import javax.swing.event.*;
+
+public class TestGameFile implements ActionListener{
+    // Properties
+    JFrame theFrame = new JFrame("Rock Paper Scissors Lizard Spock");
+    HomeScreen thePanel = new HomeScreen();
+    JButton serverButton = new JButton("Host Game");
+    JButton clientButton = new JButton("Join Game");
+
+    // Methods
+    public void actionPerformed(ActionEvent evt) {
+        if(evt.getSource() == serverButton){
+            System.out.println("Hosting Game");
+        }else if(evt.getSource() == clientButton){
+            System.out.println("Joining Game");
+        }
+    }
+
+    // Constructor
+    public TestGameFile(){
+        thePanel.setPreferredSize(new Dimension(1280, 720));
+        thePanel.setLayout(null);
+        theFrame.setContentPane(thePanel);
+        theFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        theFrame.pack();
+        theFrame.setVisible(true);
+
+        serverButton.setSize(880, 100);
+        serverButton.setLocation(200, 500);
+        serverButton.addActionListener(this);
+
+        clientButton.setSize(880, 100);
+        clientButton.setLocation(200, 610);
+        clientButton.addActionListener(this);
+
+        thePanel.add(serverButton);
+        thePanel.add(clientButton);
+    }
+
+    // Main Method
+    public static void main(String[] args){
+        TestGameFile tgf = new TestGameFile();
+    }
 }
