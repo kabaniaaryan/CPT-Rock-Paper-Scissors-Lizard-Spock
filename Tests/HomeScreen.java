4b8afacd4856a72a452eafd1197f6ff6502d0405
CLee020 <Carter.lee26@ycdsbk12.ca>
2026-01-12 09:57:23 -0500

    Buttons

--- Tests/HomeScreen.java
+++ Tests/HomeScreen.java
@@ -1,4 +1,4 @@
-package Tests;
+
 
 import java.awt.*;
 import javax.swing.*;
@@ -7,18 +7,44 @@ import javax.swing.event.*;
 
 public class HomeScreen implements ActionListener {
 
-         // Properties
+        // Properties
         JFrame theFrame = new JFrame("Rock Paper Scissors Lizard Spock");
-        
-    
+        JPanel thePanel = new JPanel();
+        JTextField PlayerTextField1 = new JTextField("P1");
+        JButton theButton1 = new JButton("Rock");
+        JButton theButton2 = new JButton("Paper");
+        JButton theButton3 = new JButton("Scissors");
+        JButton theButton4 = new JButton("Lizard");
+        JButton theButton5 = new JButton("Spock");
+
+        // Array
+        String[] strChoiceNames = {"Rock", "Paper", "Scissors", "Lizard", "Spock"};
+
+        // Math
+
 
         // Methods
         public void actionPerformed(ActionEvent evt) {
-            
+            if(evt.getSource() == theButton1) {
+
+
+            } else if(evt.getSource() == theButton2) {
+
+                
+            } else if(evt.getSource() == theButton3) {
+
+                
+            } else if(evt.getSource() == theButton4) {
+
+                
+            } else if(evt.getSource() == theButton5) {
+
+                
+            }
         }
         // Constructor
         public HomeScreen() {
-
+            new HomeScreen();
 
         }
         
