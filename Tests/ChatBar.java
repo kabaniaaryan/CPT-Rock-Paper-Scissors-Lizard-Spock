f45d763d0f9e80b2477dffc1010871772a016ff4
AidanTheThe <aidan.brown26@ycdsbk12.ca>
2026-01-14 09:36:17 -0500

    Started work on chat bar

--- Tests/ChatBar.java
+++ Tests/ChatBar.java
@@ -5,37 +5,52 @@ import javax.swing.*;
 import java.awt.event.*;
 import javax.swing.event.*;
 
-public class ChatBar {
+public class ChatBar implements ActionListener{
     // Properties
     JFrame theFrame = new JFrame("GAME");
-    QuarterFinal qfPanel = new QuarterFinal();
+    HomeScreen homePanel = new HomeScreen();
     JButton serverButton = new JButton("Host Game");
     JButton clientButton = new JButton("Join Game");
     JTextField ipField = new JTextField();
+    JTextField messageField = new JTextField();
+    JTextArea chatArea = new JTextArea();
+    JScrollPane theScroll = new JScrollPane(chatArea);
 
     // Methods
+    public void actionPerformed(ActionEvent evt) {
+        // To be implemented
+    }
 
     // Constructor
     public ChatBar(){
         // Frame & Panel
-        qfPanel.setPreferredSize(new Dimension(1280,720));
-        qfPanel.setLayout(null);
-        theFrame.setContentPane(qfPanel);
+        homePanel.setPreferredSize(new Dimension(1280,720));
+        homePanel.setLayout(null);
+        theFrame.setContentPane(homePanel);
         theFrame.pack();
         theFrame.setVisible(true);
 
         // Buttons, TextFields, TextAreas, ScrollPanes
         serverButton.setSize(100, 100);
         serverButton.setLocation(0, 620);
-        qfPanel.add(serverButton);
+        homePanel.add(serverButton);
 
         clientButton.setSize(100, 100);
         clientButton.setLocation(100, 620);
-        qfPanel.add(clientButton);
+        homePanel.add(clientButton);
 
         ipField.setSize(100, 100);
         ipField.setLocation(200, 620);
-        qfPanel.add(ipField);
+        homePanel.add(ipField);
+
+        messageField.setSize(300, 40);
+        messageField.setLocation(980, 680);
+        homePanel.add(messageField);
+
+        theScroll.setSize(300, 500);
+        theScroll.setLocation(980, 100);
+        chatArea.setEditable(false);
+        homePanel.add(theScroll);
     }
 
     // Main Method
