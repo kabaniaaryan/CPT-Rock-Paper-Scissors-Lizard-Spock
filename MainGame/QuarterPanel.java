595528df2cbf4e7b5a186cef1847c067f2499081
AidanTheThe <aidan.brown26@ycdsbk12.ca>
2026-01-16 08:30:42 -0500

    added images and QuarterPanel to maingame directory

---
+++ MainGame/QuarterPanel.java
@@ -0,0 +1,32 @@
+package MainGame;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.io.IOException;
+import java.io.InputStream;
+import java.awt.image.*;
+import javax.imageio.ImageIO;
+import java.awt.Graphics;
+
+public class QuarterPanel extends JPanel{
+    //Properties
+    BufferedImage qfImage;
+
+    //Methods
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        g.drawImage(qfImage, 0, 0, null);
+    }
+
+    //Constructor
+    public QuarterPanel(){
+        super();
+        InputStream imageStream = null;
+        imageStream = this.getClass().getResourceAsStream("Quarters.png");
+        try{
+            qfImage = ImageIO.read(imageStream);
+        }catch(IOException e){
+            System.out.println("Error loading image");
+        }
+    }
+}
