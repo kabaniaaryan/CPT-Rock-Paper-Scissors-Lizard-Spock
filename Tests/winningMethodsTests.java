d25c12602f92e3743d007862a184a1f2c924eca8
kabaniaaryan <kabaniaaryan@gmail.com>
2026-01-16 08:59:52 -0500

    created winning methods test file

---
+++ Tests/winningMethodsTests.java
@@ -0,0 +1,29 @@
+package Tests;
+
+
+public class winningMethodsTests {
+   public static String isWinner(String playerChoice, String opponentChoice) {
+       if (playerChoice.equals(opponentChoice)) {
+           return "t";
+       }
+
+
+       switch (playerChoice) {
+           case "Rock":
+               return (opponentChoice.equals("Scissors")) ? "w" : "l";
+           case "Paper":
+               return (opponentChoice.equals("Rock")) ? "w" : "l";
+           case "Scissors":
+               return (opponentChoice.equals("Paper")) ? "w" : "l";
+           case "Lizard":
+               return (opponentChoice.equals("Spock") || opponentChoice.equals("Paper")) ? "w" : "l";
+           case "Spock":
+               return (opponentChoice.equals("Scissors") || opponentChoice.equals("Rock")) ? "w" : "l";
+           default:
+               return "Invalid choice!";
+       }
+   }
+}
+
+
+
